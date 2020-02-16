04ef353e7b2c1f3b0be1e69f5ce857628543510e
Jacek Szczepański <jacogitproject@gmail.com>
2020-02-16 16:44:23 +0100

    Extend functionality to create UnitTest class

---
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczneTests/KolejkaKolowaTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _1_TypyGeneryczneTests
+{
+    [TestClass]
+    public class KolejkaKolowaTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ CsharpStrukturyGeneryczne/1_TypyGeneryczneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("1_TypyGeneryczneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1_TypyGeneryczneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a64d6983-9da6-47a5-8dde-f79d827a3677")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
