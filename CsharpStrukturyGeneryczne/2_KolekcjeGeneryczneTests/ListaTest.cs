5f8997f905cbb3ce03bfe00d4fe66d7e93ffc9e2
Jacek Szczepański <jacogitproject@gmail.com>
2020-03-01 22:51:11 +0100

    Test to List<T>

---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTests/ListaTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_KolekcjeGeneryczneTests
+{
+    [TestClass]
+    public class ListaTest
+    {
+        [TestMethod]
+        public void ListaMozemyDodawacNaKoniec()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+            listaLiczb.Add(4);
+
+            Assert.AreEqual(4, listaLiczb[3]);
+        }
+        [TestMethod]
+        public void ListaMozemyDodawacNaOkreslonejPozycji()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+            listaLiczb.Insert(2,5);
+
+            Assert.AreEqual(5,listaLiczb[2]);
+        }
+
+        [TestMethod]
+        public void ListaUsuwanieElementuZlisty()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+            listaLiczb.Remove(2);
+
+            Assert.IsTrue(listaLiczb.SequenceEqual(new List<int>{1,3}),"Usunięto liczbe 2");
+        }
+
+        [TestMethod]
+        public void ListaUsuwanieElementuZkonkretnejPozycji()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+            listaLiczb.RemoveAt(1);
+
+            Assert.IsTrue(listaLiczb.SequenceEqual(new []{1,3}));
+        }
+
+        [TestMethod]
+        public void ListaMozemyWyszukiwac()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+
+
+            Assert.AreEqual(listaLiczb.IndexOf(3),2);
+        }
+
+        [TestMethod]
+        public void ListaCzyZawiera()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+
+
+            Assert.IsTrue(listaLiczb.Contains(3));
+        }
+        [TestMethod]
+        public void ListaMozemyDodawacZakresLiczb()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+            listaLiczb.AddRange(new List<int>{4,5,6});
+
+            Assert.AreEqual(listaLiczb.IndexOf(4), 3);
+        }
+    }
+}
---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("2_KolekcjeGeneryczneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2_KolekcjeGeneryczneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e501689-bef6-49ba-ae7a-bae7738a3152")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
