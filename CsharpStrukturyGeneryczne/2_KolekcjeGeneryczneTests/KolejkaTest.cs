69371704eb07980bd54e2652148a4a1e46847190
Jacek Szczepański <jacogitproject@gmail.com>
2020-03-04 21:50:35 +0100

    Add Queue (koejka)

---
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczneTests/KolejkaTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_KolekcjeGeneryczneTests
+{
+    [TestClass]
+    public class KolejkaTest
+    {
+        private Queue<int> kolejka;
+        public KolejkaTest()
+        {
+            kolejka = new Queue<int>();
+            ZasilKolejke();
+        }
+
+        private void ZasilKolejke()
+        {
+            kolejka.Enqueue(1);
+            kolejka.Enqueue(2);
+            kolejka.Enqueue(3);
+            kolejka.Enqueue(4);
+        }
+        [TestMethod]
+        public void UzycPeek()
+        {
+            Assert.AreEqual(1, kolejka.Peek());
+        }
+
+        [TestMethod]
+        public void UzycieContains()
+        {
+            Assert.IsTrue(kolejka.Contains(3));
+        }
+
+        [TestMethod]
+        public void ToArray()
+        {
+            var tablica = kolejka.ToArray();
+            Assert.AreEqual(1,tablica[0]);
+            kolejka.Dequeue();
+            Assert.AreEqual(1, tablica[0]);
+            Assert.AreEqual(3,kolejka.Count);
+        }
+
+        [TestMethod]
+        public void UzycieClear()
+        {
+            kolejka.Clear();
+            Assert.AreEqual(0,kolejka.Count);
+        }
+    }
+}
